f71a8ca060336dff06af7d36064580347b6f7b29
Aditya <adirkmv2019@gmail.com>
2023-12-02 18:17:57 +0530

    Added utility functions

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv"
+
+import connectDB from "./db/index.js"
+//require('dotenv').config({path:'./env'})
+
+dotenv.config()
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||3000,()=>{
+        console.log(`Server running on port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection Error",err);
+})
